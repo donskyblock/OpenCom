baf1e5b21c234b43a13d054b57f56a7573d3e7f1
don <heisgenburgthecook@gmail.com>
2026-02-16 00:51:37 +0000

    Feat: Add Basic Command + placeholder funcs

--- Extensions/Server/skyblock-utils/index.js
+++ Extensions/Server/skyblock-utils/index.js
@@ -1,5 +1,19 @@
 import { command, createServerContext, optionString } from "../../lib/opencom-extension-sdk.js";
 
+
+function getSkillLevel(username) {
+    return 0
+}
+
+function getsSlayerLevel(username, slayer) {
+    return 0
+}
+
+function getDungeonLevel(username) {
+    return 0
+}
+
+
 export const commands = [
   command({
     name: "sb-plugin",
@@ -14,7 +28,57 @@ export const commands = [
         serverId: ctx.serverId
       };
     }
-  })
+  }),
+  command({
+    name: "slayer",
+    description: "Simple command to test the extension",
+    options: [optionString("username", "Minecraft username", true), optionString("slayer", "Which slayer boss (zombie, spider, wolf)", true)],
+    async execute(ctx) {
+        const username = String(ctx.args?.username);
+        const slayer = String(ctx.args?.slayer);
+        const slayerLevel = getsSlayerLevel(username, slayer);
+        const input = `${username}'s ${slayer} slayer level is ${slayerLevel}`;
+        const me = await ctx.apis.node.get("/v1/me").catch(() => null);
+      return {
+        content: `${input}`,
+        user: me?.user?.username || ctx.userId,
+        serverId: ctx.serverId
+      };
+    }
+  }),
+  command({
+    name: "skill",
+    description: "Get a player's skill level",
+    options: [optionString("username", "Minecraft username", true), optionString("skill", "Which skill", true)],
+    async execute(ctx) {
+        const username = String(ctx.args?.username);
+        const skill = String(ctx.args?.slayer);
+        const skillLevel = getsSlayerLevel(username, skill);
+        const input = `${username}'s ${skill} level is ${skillLevel}`;
+        const me = await ctx.apis.node.get("/v1/me").catch(() => null);
+        return {
+        content: `${input}`,
+        user: me?.user?.username || ctx.userId,
+        serverId: ctx.serverId
+      };
+    }
+  }),
+  command({
+    name: "dungeon",
+    description: "Get a player's catacombs level",
+    options: [optionString("username", "Minecraft username", true)],
+    async execute(ctx) {
+        const username = String(ctx.args?.username);
+        const cata = getDungeonLevel(username);
+        const input = `${username} is catacombs ${cata}`;
+        const me = await ctx.apis.node.get("/v1/me").catch(() => null);
+        return {
+        content: `${input}`,
+        user: me?.user?.username || ctx.userId,
+        serverId: ctx.serverId
+      };
+    }
+  }),
 ];
 
 export async function activate(ctx) {
