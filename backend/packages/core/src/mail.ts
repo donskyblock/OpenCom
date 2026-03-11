28465a5af5a4a27cb79523a6f468f9694558a695
don <heisgenburgthecook@gmail.com>
2026-03-11 13:27:22 +0000

    Refactor: change the unreecognised singi n message slightly

--- backend/packages/core/src/mail.ts
+++ backend/packages/core/src/mail.ts
@@ -22,3 +22,13 @@ export async function sendPasswordResetEmail(to: string, resetToken: string) {
     html: `<p>We received a request to reset your OpenCom password.</p><p>Set a new password by opening this link:</p><p><a href="${resetUrl}">${resetUrl}</a></p><p>If you did not request this, you can ignore this email.</p>`
   });
 }
+
+export async function sendSigninEmail(to: string, ip: string) {
+  const base = env.APP_BASE_URL.replace(/\/$/, "");
+  const loginUrl = `${base}/login`
+  await sendSmtpEmail({
+    to,
+    subject: "OpenCom Unrecognised Signin Alert",
+    text: `There has been a Unrecognised sign in to your account.\nThe ip of this sign in is ${ip}\nIf you do not recognise this sign in please login at ${loginUrl} and reset your paassword.\nIf this was you you can safely ignore this email!`,
+  })
+}
